a9d502d5a6b6ecf1bef86b4a5ccaa71d8e42fc04
Mateus Pandino <matpandino@gmail.com>
2024-01-17 19:58:16 -0300

    wip: app layout component

--- src/app/rider/page.tsx
+++ src/app/rider/page.tsx
@@ -1,7 +1,21 @@
+import { Layout } from "@/components/ui/app-layout"
+import { SearchTrip } from "./_components/search-trip"
+
 export default function Page() {
     return (
-        <div>
-            <h1>Rider Page</h1>
-        </div>
+        <Layout
+            header={<>Goober</>}
+            leftContent={
+                <div className="flex flex-col w-full gap-2 bg-background">
+                    <SearchTrip />
+                </div>
+            }
+            rightContent={
+                <div>
+                    TODO: Map integration
+                </div>
+            }
+        />
+
     )
 }
---
+++ src/components/ui/app-layout.tsx
@@ -0,0 +1,51 @@
+import { ReactNode } from "react";
+
+interface LayoutProps {
+    header: ReactNode
+    leftContent?: ReactNode
+    rightContent?: ReactNode
+}
+
+export const Layout = ({ header, leftContent, rightContent }: LayoutProps) => {
+    return (
+        <div className="flex h-screen flex-col w-full bg-background">
+            <header className="bg-background w-full px-2">
+                <nav className="mx-auto flex max-w-7xl items-center justify-between p-3 lg:px-8" aria-label="Global">
+                    {header}
+                    {/* <div className="flex lg:flex-1">
+                        <a href="#" className="-m-1.5 p-1.5">
+                            <span className="text-lg text-pretty">Goober</span>
+                        </a>
+                    </div>
+                    <div className="flex lg:flex-1">
+                        <a href="#" className="-m-1.5 p-1.5">
+                            <span className="text-md text-pretty">Log out</span>
+                        </a>
+                    </div>
+
+                    <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+                        <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                            Log in <span aria-hidden="true">&rarr;</span>
+                        </a>
+                    </div> */}
+                </nav>
+            </header>
+            <main className="h-full w-full flex p-4 gap-4">
+                {leftContent && (
+                    <div className="h-full w-2/5 max-w-lg flex bg-background">
+                        <div className="flex-1 flex rounded-lg bg-slate-100">
+                            {leftContent}
+                        </div>
+                    </div>
+                )}
+                {rightContent && (
+                    <div className="h-full flex-1 w-auto flex bg-background">
+                        <div className="flex-1 flex rounded-lg bg-slate-200">
+                            {rightContent}
+                        </div>
+                    </div>
+                )}
+            </main>
+        </div>
+    );
+};
