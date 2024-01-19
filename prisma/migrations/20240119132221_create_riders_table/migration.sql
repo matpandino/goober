64642044e77eb955ae5327094ff8cdb4ad731a54
Mateus Pandino <matpandino@gmail.com>
2024-01-19 11:38:20 -0300

    add prisma + create schema and Rider table migration

---
+++ prisma/migrations/20240119132221_create_riders_table/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Rider" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Rider_pkey" PRIMARY KEY ("id")
+);
