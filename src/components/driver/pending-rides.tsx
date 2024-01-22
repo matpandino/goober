5a8afa3d5ab170e3dbf6b4c1763fdc27464100e9
Mateus Pandino <matpandino@gmail.com>
2024-01-22 19:58:37 -0300

    enhance pending rides ui

--- src/components/driver/pending-rides.tsx
+++ src/components/driver/pending-rides.tsx
@@ -1,8 +1,10 @@
 'use client'
 
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { kmFormatter, moneyFormatter } from '@/lib/utils'
 import { type Ride } from '@/types'
 import { useQuery } from '@tanstack/react-query'
+import { useState } from 'react'
 import useRideActions from '../../hooks/use-ride-actions'
 import { useUser } from '../providers/user-provider'
 import { Button } from '../ui/button'
@@ -10,6 +12,7 @@ import { Button } from '../ui/button'
 const PendingRides = () => {
   const { driver } = useUser()
   const { acceptRide } = useRideActions()
+  const [isLoadingAccept, setIsLoadingAccept] = useState(false)
 
   const { isLoading, data, refetch } = useQuery({
     queryKey: ['pendingRides'],
@@ -23,7 +26,9 @@ const PendingRides = () => {
       console.error('Driver not found')
       return
     }
+    setIsLoadingAccept(true)
     await acceptRide(ride, driver.id)
+    setIsLoadingAccept(false)
   }
 
   return (
@@ -38,11 +43,19 @@ const PendingRides = () => {
               {data?.rides?.map((ride: Ride) => (
                 <li
                   key={ride.id}
-                  className="flex px-6 py-4 text-sm justify-between items-center"
+                  className="flex px-6 py-4 gap-2 text-sm justify-between items-center"
                 >
-                  <span>{ride.rider.name} is pending a ride</span>
+                  <div className='flex-col'>
+                    <span><b>{ride.rider.name}</b> is pending a <b>{kmFormatter.format(ride.distance / 1000)}</b> ride</span>
+                    <br />
+                    <span className='text-slate-500 text-xs'><b>From:</b> <i>{ride.fromName}</i></span>
+                    <br />
+                    <span className='text-slate-500 text-xs'><b>To:</b> <i>{ride.toName}</i></span>
+                    <br />
+                    <span className='text-base text-green-700'>{moneyFormatter.format(ride.price / 100)}</span>
+                  </div>
                   <Button
-                    disabled={isLoading}
+                    disabled={isLoadingAccept}
                     onClick={() => handleAcceptRide(ride)}
                   >
                     Accept
@@ -54,7 +67,7 @@ const PendingRides = () => {
         )}
         {isLoading && <span>Loading</span>}
         {!isLoading && data.rides?.length === 0 && (
-          <span>No pending rides found</span>
+          <span className='text-slate-500'>No pending rides at the moment</span>
         )}
       </CardContent>
     </Card>
